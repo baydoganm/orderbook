93a103d195dcdb4f71ac4dd5be1caf05337cfb63
freebird <monsiper@gmail.com>
2015-05-28 00:17:51 +0300

    updated scrape.py

--- scrape.py
+++ scrape.py
@@ -3,14 +3,13 @@ import finance,time,tablib,csv
 from datetime import datetime
 
 
-#data=tablib.Dataset()
-headers=['Index','Date','Time','Best Bid','Bid Amount','Best Ask','Ask Amount']
-#data.headers=['Index','Date','Time','Best Bid','Bid Amount','Best Ask','Ask Amount']
-freq=10     #sets the sampling rate
-endInd=168*60*(60/freq)  #sets total record time
 
-with open('data4.csv','w') as f:
-    writer=csv.writer(f,dialect="excel")
+headers=['Index','Date','Time','Best Bid','Bid Amount','Best Ask','Ask Amount']
+freq=10     #sets the sampling rate
+endInd=1*1*(60/freq)  #sets total record time
+
+with open('data4.csv','wb') as f:
+    writer=csv.writer(f,dialect='excel')
     writer.writerow(headers)
 
     for ind in range(1,endInd+1):
@@ -18,22 +17,23 @@ with open('data4.csv','w') as f:
         locTime1=datetime.now()
         (dt,tm)=(locTime1.strftime('%Y%m%d&%H%M%S')).split('&')
         if (entData[0]==True):
-            relData=[ind,dt,tm,float(entData[1]['bids'][0][0]),float(entData[1]['bids'][0][1]),
+            try:
+                relData=[ind,dt,tm,float(entData[1]['bids'][0][0]),float(entData[1]['bids'][0][1]),
                        float(entData[1]['asks'][0][0]),float(entData[1]['asks'][0][1])]
+            except KeyError:
+                relData=[ind,dt,tm,'?','?','?','?']
+        elif (entData[0]==False and entData[1]==1):
+            relData=[ind,dt,tm,'?','?','?','?']
         else:
             relData=[ind,dt,tm,'?','?','?','?']
-			
-        #data.append(relData)
+            print entData[1]
         writer.writerow(relData)
 	print "%s\n" % relData
         time.sleep(freq)
 		
-
 f.close()
 
 
-
-#
 # stTime=t1   #record start time
 # dataInd=1
 # relData=(int(dataInd),dt, tm,float(entData['bids'][0][0]),float(entData['bids'][0][1]),
@@ -67,20 +67,3 @@ f.close()
 
 
 
-
-
-#fatih_portfolio = [["aapl", 100], ["msft", 300], ["intc", 250]]
-
-# print finance.calculate_total_portfolio(fatih_portfolio)
-#
-#
-# ustun_portfolio = [["aapl", 150], ["twtr", 300], ["fb", 250]]
-#
-# #print calculate_total_portfolio(ustun_portfolio)
-#
-
-
-
-
-#print get_stock_price("aapl")
-# print get_stock_price("twtr")
